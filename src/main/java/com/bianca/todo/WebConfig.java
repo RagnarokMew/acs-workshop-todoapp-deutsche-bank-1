cd2bdc30780f20db454fa59995aa3af1eb70b96a
biancaradu13 <andreea103bianca@yahoo.com>
2024-12-05 12:09:58 +0200

    web config

--- src/main/java/com/bianca/todo/WebConfig.java
+++ src/main/java/com/bianca/todo/WebConfig.java
@@ -1,19 +1,25 @@
 package com.bianca.todo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
+
+    private static final Logger logger = LoggerFactory.getLogger(WebConfig.class);
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")  // Allow all paths
-                .allowedOrigins("http://example.com") // Replace with your domain
+        logger.info("startiing web");
+        registry.addMapping("/**")// Allow all paths
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(true); // Allow cookies
+                .allowCredentials(true);
     }
 }
